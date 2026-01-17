a2730260409693c31e5f9e96754070d6e91eeb7f
Daphne Plott <daphneplott@gmail.com>
2026-01-16 19:44:52 -0700

    King move and Bishop Move

--- shared/src/main/java/chess/BishopMove.java
+++ shared/src/main/java/chess/BishopMove.java
@@ -1,4 +1,85 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BishopMove {
+    private ArrayList<ChessMove> moves = new ArrayList<>();
+
+    public BishopMove(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
+        /*
+        General notes I want to copy to all things:
+        - Don't move off the board
+        - You can't capture a piece of your color
+
+        A bishop moves on a diagonal
+        A bishop can't move past objects, but can capture a piece in its path
+        Math
+        - +-x paired with +-x
+
+         */
+
+        ChessPosition next;
+        ChessPiece piece;
+        /*
+        This first for loop with go diagonals up and to the right (both positive)
+         */
+        for (int x = 1; x < 8; x++) {
+            if (position.getRow() + x < 9 && position.getColumn() + x < 9) {
+                next = new ChessPosition(position.getRow() + x, position.getColumn() + x);
+                piece = board.getPiece(next);
+                if (piece == null) {
+                    moves.add(new ChessMove(position, next, null));
+                } else if (piece.getTeamColor() != color) {
+                    moves.add(new ChessMove(position, next, null));
+                    break;
+                } else {break;}
+            } else {break;}
+        }
+        /*
+        This second for loop will go with diagonals up and to the left (+, -)
+         */
+        for (int x = 1; x < 8; x++) {
+            if (position.getRow() + x < 9 && position.getColumn() - x > 0) {
+                next = new ChessPosition(position.getRow() + x, position.getColumn() - x);
+                piece = board.getPiece(next);
+                if (piece == null) {
+                    moves.add(new ChessMove(position, next, null));
+                } else if (piece.getTeamColor() != color) {
+                    moves.add(new ChessMove(position, next, null));
+                    break;
+                } else {break;}
+            } else {break;}
+        }
+        /*
+        This third for loop will do diagonals down and to the right (-, +)
+         */
+        for (int x = 1; x < 8; x++) {
+            if (position.getRow() - x > 0 && position.getColumn() + x < 9) {
+                next = new ChessPosition(position.getRow() - x, position.getColumn() + x);
+                piece = board.getPiece(next);
+                if (piece == null) {
+                    moves.add(new ChessMove(position, next, null));
+                } else if (piece.getTeamColor() != color) {
+                    moves.add(new ChessMove(position, next, null));
+                    break;
+                } else {break;}
+            } else {break;}
+        }
+        /*
+        This fourth for loop will do diagonals down and to the left (both negative)
+         */
+        for (int x = 1; x < 8; x++) {
+            if (position.getRow() -x >0 && position.getColumn() -x >0) {
+                next = new ChessPosition(position.getRow() - x, position.getColumn() - x);
+                piece = board.getPiece(next);
+                if (piece == null) {
+                    moves.add(new ChessMove(position, next, null));
+                } else if (piece.getTeamColor() != color) {
+                    moves.add(new ChessMove(position, next, null));
+                    break;
+                } else {break;}
+            } else {break;}
+        }
+    }
 }
--- shared/src/main/java/chess/KingMove.java
+++ shared/src/main/java/chess/KingMove.java
@@ -1,4 +1,42 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class KingMove {
+    private ArrayList<ChessMove> moves = new ArrayList<>();
+
+    record Pair(int x, int y) {}
+
+    public KingMove(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
+        /*
+        General notes I want to copy to all things:
+        - Don't move off the board
+        - You can't capture a piece of your color
+
+        A king can move one square in any direction, include diagonal.
+        Math:
+         - All pairs of +-1 or 0
+
+         */
+        ArrayList<Pair> potential_math = new ArrayList<Pair>(Arrays.asList(
+                new Pair(1,0), new Pair(1,1), new Pair(1,-1),
+                new Pair(-1,0),new Pair(-1,1), new Pair(1,-1),
+                new Pair(0,1), new Pair(0,-1)
+        ));
+        ChessPosition next;
+        ChessPiece piece;
+        for (Pair pair : potential_math) {
+            if (position.getColumn() + pair.y() < 9 && position.getColumn() + pair.y() >0 &&
+                    position.getRow() + pair.x() < 9&& position.getRow()+pair.x() > 0
+            ) {
+                next = new ChessPosition(position.getRow() + pair.x(), position.getColumn() + pair.y());
+                piece = board.getPiece(next);
+                if (piece == null || piece.getTeamColor() != color) {
+                    moves.add(new ChessMove(position, next, null));
+                }
+            }
+        }
+
+    }
 }
