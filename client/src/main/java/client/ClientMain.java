ebacb9bfb22ce070d50605918fd9e8ef35edd30c
Daphne Plott <daphneplott@gmail.com>
2026-03-15 17:57:43 -0600

    unit tests

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,6 +1,7 @@
 package client;
 
 import chess.*;
+import ui.ChessClient;
 
 public class ClientMain {
     public static void main(String[] args) {
@@ -8,5 +9,17 @@ public class ClientMain {
         // If given the args, change serverUrl
         // Create client object
         // Do something like client.run()
+
+        String serverUrl = "http://localhost:8080";
+        if (args.length == 1) {
+            serverUrl = args[0];
+        }
+
+        try {
+            new ChessClient(serverUrl).run();
+
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
     }
 }
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,36 @@
 package client;
 
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
 import org.junit.jupiter.api.*;
 import server.Server;
+import ui.ServerFacade;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
+    static String url;
+    static HttpClient client;
+    AuthData auth;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        url = String.format("http://localhost:%d", port);
+        facade = new ServerFacade(url);
+        client = HttpClient.newHttpClient();
     }
 
     @AfterAll
@@ -20,10 +38,109 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    public void clearServer() throws IOException, InterruptedException {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(url + "/db"))
+                .method("DELETE", HttpRequest.BodyPublishers.ofString(""));
+        var builtRequest = request.build();
+        var response = client.send(builtRequest, HttpResponse.BodyHandlers.ofString());
+        Assertions.assertEquals(200,response.statusCode());
 
-    @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+        var response2 = facade.register(new String[]{"register", "user", "1234", "user@email.com"});
+        auth = response2.auth();
     }
 
+
+    @Test
+    public void registerSuccess() {
+        var response = facade.register(new String[]{"register","new user","5678","newuser@mail.com"});
+        Assertions.assertEquals(200,response.responseCode());
+        Assertions.assertTrue(response.auth().authToken().length() > 10);
+    }
+
+    @Test
+    public void registerFailure() {
+        var response = facade.register(new String[]{"register","user","5678","different@mail.com"});
+        Assertions.assertEquals(403,response.responseCode());
+        Assertions.assertTrue(response.auth().authToken().length() < 2);
+    }
+
+    @Test
+    public void logoutSuccess() {
+        var response = facade.logout(auth);
+        Assertions.assertEquals(200,response.responseCode());
+    }
+
+    @Test
+    public void logoutFailure() {
+        var response = facade.logout(new AuthData("7777","fake username"));
+        Assertions.assertEquals(401,response.responseCode());
+    }
+
+    @Test
+    public void loginSuccess() {
+        facade.logout(auth);
+        var response = facade.login(new String[]{"login","user","1234"});
+        Assertions.assertEquals(200,response.responseCode());
+        Assertions.assertTrue(response.auth().authToken().length() > 10);
+    }
+
+    @Test
+    public void loginFailure() {
+        facade.logout(auth);
+        var response = facade.login(new String[]{"login","user","5678"});
+        Assertions.assertEquals(401,response.responseCode());
+        Assertions.assertTrue(response.auth().authToken().length() < 2);
+    }
+
+    @Test
+    public void createSuccess() {
+        var response = facade.create(new String[]{"create","cool name"},auth);
+        Assertions.assertEquals(200,response.responseCode());
+        Assertions.assertEquals(1,response.gameID());
+    }
+
+    @Test
+    public void createFailure() {
+        var response = facade.create(new String[]{"create","cool name"}, new AuthData("1234","user"));
+        Assertions.assertEquals(401,response.responseCode());
+        Assertions.assertEquals(-1,response.gameID());
+    }
+
+    @Test
+    public void listSuccess() {
+        facade.create(new String[]{"create","cool name"},auth);
+        facade.create(new String[]{"create","cool name 2"},auth);
+        var response = facade.list(auth);
+        Assertions.assertEquals(200,response.responseCode());
+        ArrayList<GameData> expected = new ArrayList<>();
+        expected.add(new GameData(1,null,null,"cool name",new ChessGame()));
+        expected.add(new GameData(2,null,null,"cool name 2",new ChessGame()));
+        Assertions.assertEquals(expected,response.list());
+    }
+
+    @Test
+    public void listFailure() {
+        var response = facade.list(new AuthData("1234","user"));
+        Assertions.assertEquals(401,response.responseCode());
+        Assertions.assertEquals(new ArrayList<GameData>(), response.list());
+    }
+
+    @Test
+    public void joinSuccess() {
+        facade.create(new String[]{"create","cool name"}, new AuthData("1234","user"));
+        var response = facade.join(new String[]{"join","white","1"},auth);
+        Assertions.assertEquals(200,response.responseCode());
+    }
+
+    @Test
+    public void joinFailure() {
+        facade.create(new String[]{"create","cool name"}, new AuthData("1234","user"));
+        facade.join(new String[]{"join","white","1"},auth);
+        var response1 = facade.register(new String[]{"register","new user","5678","newuser@mail.com"});
+        AuthData newAuth = response1.auth();
+        var response = facade.join(new String[]{"join","white","1"},newAuth);
+        Assertions.assertEquals(403,response.responseCode());
+    }
 }
